e7ffaaa642da3087f139d27b898a9f2f6f10ad8e
ALIENQuake <alienquake@hotmail.com>
2018-12-18 12:01:23 +0100

    cosmetic

--- CreateNewGithubRelease.ps1
+++ CreateNewGithubRelease.ps1
@@ -1,41 +1,42 @@
 
 function New-GithubReleaseDescription {
-    
-    Add-Type -assembly System.Windows.Forms
-    $form=New-Object System.Windows.Forms.Form
-    $form.StartPosition='CenterScreen'
-    $form.Height='600'
-    $form.Width='800'
 
-    $btn=New-Object System.Windows.Forms.Button
-    $btn.Text='Save'
-    $btn.DialogResult='Ok'
-    $btn.Dock='bottom'
-    $form.Controls.Add($btn)
-    $btnCancel=New-Object System.Windows.Forms.Button
-    $btnCAncel.Text='Cancel'
-    $btnCancel.DialogResult='Cancel'
-    $btnCancel.Dock='bottom'
-    $form.Controls.Add($btnCancel)
-    $tb=New-Object System.Windows.Forms.Textbox
-    $tb.Multiline=$true
-    $tb.Dock='Fill'
-    $form.Controls.Add($tb)
-    $form.add_load({$tb.Select()})
-    if($form.ShowDialog() -eq 'Ok'){
+    Add-Type -AssemblyName System.Windows.Forms
+    $form = New-Object System.Windows.Forms.Form
+    $form.StartPosition = 'CenterScreen'
+    $form.Height = '600'
+    $form.Width = '800'
+
+    $button = New-Object System.Windows.Forms.Button
+    $button.Text = 'Save'
+    $button.DialogResult = 'Ok'
+    $button.Dock = 'bottom'
+    $form.Controls.Add( $button )
+    $buttonCancel = New-Object System.Windows.Forms.Button
+    $buttonCAncel.Text = 'Cancel'
+    $buttonCancel.DialogResult = 'Cancel'
+    $buttonCancel.Dock = 'bottom'
+    $form.Controls.Add($buttonCancel)
+    $textBox = New-Object System.Windows.Forms.Textbox
+    $textBox.Multiline = $true
+    $textBox.Dock = 'Fill'
+    $form.Controls.Add( $textBox )
+    $form.add_load( { $textBox.Select() } )
+    if ( $form.ShowDialog() -eq 'Ok' ) {
         $form.BringToFront()
-        return $tb.lines
-    }else{
+        return $textBox.lines
+    } else {
         Write-Host 'Cancelled' -ForegroundColor red
         return $null
     }
-
 }
 
-function Get-IEModVersion ($tp2FullPath) {
-	(( Get-Content $tp2FullPath | Select-String "version ~" ) -split '~' )[1]
+function Get-IEModVersion {
+    param( $tp2FullPath )
+    (( Get-Content $tp2FullPath | Select-String "version ~" ) -split '~' )[1]
 }
-$( try { $script:MyInvocation.MyCommand.Path, $script:psISE.CurrentFile.Fullpath, $script:psEditor.GetEditorContext().CurrentFile.Path, $script:dte.ActiveDocument.FullName } catch {} ) | % { $_ | Split-Path -EA 0 | Set-Location }
+
+$( try { $script:MyInvocation.MyCommand.Path, $script:psISE.CurrentFile.Fullpath, $script:psEditor.GetEditorContext().CurrentFile.Path, $script:dte.ActiveDocument.FullName } catch { $_ } ) | % { $_ | Split-Path -EA 0 | Set-Location }
 
 # Set this to you Personal access token
 $apiKey = Get-Content "$env:USERPROFILE\CreateNewGithubRelease-APIKey.txt"
@@ -44,13 +45,13 @@ $OrgUser = (Split-Path ( git config --get remote.origin.url ) -Parent ) -replace
 $username = git config --get user.name
 
 $Token = $username + ':' + $apiKey
-$Base64Token = [System.Convert]::ToBase64String([char[]]$Token)
+$Base64Token = [System.Convert]::ToBase64String( [char[]]$Token )
 $Headers = @{ Authorization = 'Basic {0}' -f $Base64Token }
 
-[array]$dataReleases = ( Invoke-RestMethod "https://api.github.com/repos/$OrgUser/$repository/releases" -Headers $Headers -Method Get ).tag_name 
+[array]$dataReleases = ( Invoke-RestMethod "https://api.github.com/repos/$OrgUser/$repository/releases" -Headers $Headers -Method Get ).tag_name
 
 $tp2File = ( Get-ChildItem -Path $modFolder -Filter *.tp2 -Recurse )[0]
-$tp2FullPath = (( Get-ChildItem -Path $modFolder -Filter *.tp2 -Recurse )[0]).FullName
+$tp2FullPath = (( Get-ChildItem -Path $modFolder -Filter *.tp2 -Recurse )[0] ).FullName
 $tp2Version = Get-IEModVersion $tp2FullPath
 $newTagRelease = $tp2Version -replace "\s+",'_'
 
@@ -71,9 +72,7 @@ Write-Host ""
 Read-Host "Press ENTER to continue, Ctrl+c to stop" | Out-Null
 
 $releaseDescription = New-GithubReleaseDescription
-if ( $releaseDescription -eq $null ) {
-    break
-}
+if ( $null -eq $releaseDescription ) { break }
 
 $Body = @{
     tag_name = "$newTagRelease"
@@ -95,7 +94,7 @@ $fullName = Get-Item $fileName | select -ExpandProperty FullName
 $json = Invoke-RestMethod "https://api.github.com/repos/$OrgUser/$repository/releases/tags/$newTagRelease" -Headers $Headers -Method GET
 if ( $json.assets.name -eq $fileName ){
     $assertID = $json.assets.id
-    Invoke-RestMethod https://api.github.com/repos/$OrgUser/$repository/releases/assets/$assertID -Headers $Headers -Method DELETE 
+    Invoke-RestMethod https://api.github.com/repos/$OrgUser/$repository/releases/assets/$assertID -Headers $Headers -Method DELETE
 }
 
 $json = Invoke-RestMethod "https://uploads.github.com/repos/$OrgUser/$repository/releases/$releaseID/assets?name=`"$fileName`"" `
@@ -111,7 +110,7 @@ $fullName = Get-Item $fileName | select -ExpandProperty FullName
 $json = Invoke-RestMethod "https://api.github.com/repos/$OrgUser/$repository/releases/tags/$newTagRelease" -Headers $Headers -Method GET
 if ( $json.assets.name -eq $fileName ){
     $assertID = $json.assets.id
-    Invoke-RestMethod https://api.github.com/repos/$OrgUser/$repository/releases/assets/$assertID -Headers $Headers -Method DELETE 
+    Invoke-RestMethod https://api.github.com/repos/$OrgUser/$repository/releases/assets/$assertID -Headers $Headers -Method DELETE
 }
 
 $json = Invoke-RestMethod "https://uploads.github.com/repos/$OrgUser/$repository/releases/$releaseID/assets?name=`"$fileName`"" `
@@ -126,7 +125,7 @@ $fullName = Get-Item $fileName | select -ExpandProperty FullName
 $json = Invoke-RestMethod "https://api.github.com/repos/$OrgUser/$repository/releases/tags/$newTagRelease" -Headers $Headers -Method GET
 if ( $json.assets.name -eq $fileName ){
     $assertID = $json.assets.id
-    Invoke-RestMethod https://api.github.com/repos/$OrgUser/$repository/releases/assets/$assertID -Headers $Headers -Method DELETE 
+    Invoke-RestMethod https://api.github.com/repos/$OrgUser/$repository/releases/assets/$assertID -Headers $Headers -Method DELETE
 }
 
 $json = Invoke-RestMethod "https://uploads.github.com/repos/$OrgUser/$repository/releases/$releaseID/assets?name=`"$fileName`"" `
